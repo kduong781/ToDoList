b3c2b588947c381ef815eab887847a2957ac3a22
Kevin Duong <kduong5@yahoo.com>
2016-10-08 22:50:46 -0700

    added hover/ accordian effect with event delegation

--- js/home.js
+++ js/home.js
@@ -12,15 +12,47 @@
 }*/
 
 var main = function() {
-        $('#add').click(function(){
+        $('#add').click(
+          function(){
           //  var task = new Task(taskName,taskDesc);
             var taskName = $("#taskName").val();
-            var taskDesc = $("taskDesc").val();
-            $("#taskList").append("<div class='item'>" +taskName +"</div>");
+            var taskDesc = $("#taskDesc").val();
+            $("#taskList").append("<div class='item'>" +taskName +"<p class='itemDesc'>"+taskDesc+"</p></div>");
             $("#taskForm").modal("hide");
+            $("#taskName").val("");
+            $("#taskDesc").val("");
+          }
+        );
+
+        $('#taskList').on('mouseenter', '.item', function( event ) {
+            $(this).addClass("selectable");
+        }).on('mouseleave', '.item', function( event ) {
+            $(this).removeClass("selectable");
         });
-    
-    $('#taskList').selectable();
+    /*    $('.item').hover(
+          function() {
+            $(this).addClass("selectable");
+          },
+          function () {
+            $(this).removeClass("selectable");
+          }
+        );*/
+        $('#taskList').on('click','.item',function() {
+          $(this).toggleClass('selected');
+          $('> .itemDesc', this).slideToggle('slow');
+        }
+      );
+        /*
+        $('.item').click(
+          function() {
+            $(this).toggleClass('selected');
+            $('> .itemDesc', this).slideToggle('slow');
+          }
+        );
+*/
+        //$('#taskList').sortable();
+
+      // $('.taskList').selectable();
 }
 
 $(document).ready(main);
