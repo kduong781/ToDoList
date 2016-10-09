568b0b9dad013b8c8d1e44c6f132f92544e15466
Kevin Duong <kduong5@yahoo.com>
2016-10-09 01:13:05 -0700

    implemented remove tasks feature

--- js/home.js
+++ js/home.js
@@ -15,46 +15,47 @@ var main = function() {
         $('#add').click(
           function(){
           //  var task = new Task(taskName,taskDesc);
+            if($('.remove').is(":visible")) {
+                $('.remove').toggle();
+            }
             var taskName = $("#taskName").val();
             var taskDesc = $("#taskDesc").val();
-            $("#taskList").append("<div class='item'>" +taskName +"<p class='itemDesc'>"+taskDesc+"</p></div>");
+            $("#taskList").append("<div class='item'>" +taskName +"<button type='button' class='btn-xs btn-danger right remove'>Remove</button><p class='itemDesc'>"+taskDesc+"</p></div>");
             $("#taskForm").modal("hide");
             $("#taskName").val("");
             $("#taskDesc").val("");
           }
         );
 
+        $('#edit').click(
+          function(){
+              $('.remove').toggle();
+          }
+        );
 
-        $('#taskList').on('mouseenter', '.item', function( event ) {
-            $(this).addClass("selectable");
-        }).on('mouseleave', '.item', function( event ) {
-            $(this).removeClass("selectable");
-        });
-    /*    $('.item').hover(
+        $('#taskList').on('click','.remove',
           function() {
+            $(this).closest(".item").remove();
+          }
+        );
+
+        $('#taskList').on('mouseenter', '.item',
+          function( event ) {
             $(this).addClass("selectable");
-          },
-          function () {
+          }).on('mouseleave', '.item', function( event ) {
             $(this).removeClass("selectable");
           }
-        );*/
-        $('#taskList').on('click','.item',function() {
-          $('.item').removeClass('selected');
-          $(this).toggleClass('selected');
-          $('> .itemDesc', this).slideToggle('slow');
-        }
-      );
-        /*
-        $('.item').click(
+        );
+
+        $('#taskList').on('click','.item',
           function() {
+            $('.item').removeClass('selected');
             $(this).toggleClass('selected');
             $('> .itemDesc', this).slideToggle('slow');
           }
         );
-*/
-        //$('#taskList').sortable();
 
-      // $('.taskList').selectable();
+
 }
 
 $(document).ready(main);
